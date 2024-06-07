1088da571bc25f8b5c3b4d9de0f221d1ba7cdf42
crameshkambale <crameshkambale@tracelink.com>
2024-06-07 12:32:24 +0530

    commit

--- newpract/src/main/java/org/example/Main.java
+++ newpract/src/main/java/org/example/Main.java
@@ -22,10 +22,15 @@ public class Main {
         Random randomNum = new Random();
 
 
-        //method1. return a random list of integers
-        //method2. return a map of Integer, String(random string)
-        //method3. Create and print odd numbers till 100
-        //practiceImpl.method3().andThen(practiceImpl.method1().contains(practiceImpl.method2().flatMapIterable(Map::keySet)).subscribe(item -> System.out.println(item)));
+        /*method1. return a random list of integers
+        method2. return a map of Integer, String(random string)
+        method3. Create and print odd numbers till 100
+        Write a RXJava function which will call above functions as
+        Call method3
+        call method1
+        call method2 & find all the values from method2 whose keys match integers from method1
+        print the matching integers and strings
+        This should be a single rxChain. Write at least 2 unit tests. Create separate java classes if need be(not mandatory).*/
 
         /*practiceImpl.method3()
                       .andThen(Observable.defer(() -> PracticeImpl.method1())
